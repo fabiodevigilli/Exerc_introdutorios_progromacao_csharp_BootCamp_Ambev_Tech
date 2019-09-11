1413ee45a50846ded2431a0dbaac72c19bc17874
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-11 13:10:45 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        //inciando uma cosntante int
+        const int testeConst = 999;
+        
+        static void Main(string[] args)
+        {
+            string testeprimitivo = string.Empty;
+            String teste2 = string.Empty;
+            bool testComp = "123" == "123";
+            Boolean testeComp = "123" == "123";
+
+            var testeFora = string.Empty;
+            testeFora = "teste";
+            var um = "teste"; //texto
+            var dois = 't'; //char
+            var três = 9; //int
+            var quatro = 3.5; //Double
+            var cinco = 0x0f; //Hex
+
+            Console.WriteLine(testeFora);
+            Console.ReadKey();
+                }
+    }
+}
