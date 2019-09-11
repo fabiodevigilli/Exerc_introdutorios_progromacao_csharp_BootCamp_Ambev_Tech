30403e9017b818ee977a724cfee7c07a25667c5a
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-11 15:56:58 -0300

    15:56 11.09

---
+++ Matriz-Lista/Matriz-Lista/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matriz_Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arrayLinear = new string[2];
+            arrayLinear[0] = "Felipe";
+            arrayLinear[1] = "Gilmar";
+
+            List<string> listaLinear = new List<string>
+            {
+                "Felipe",
+                "Giomar",
+                "ok"
+            };
+            Console.WriteLine(listaLinear[0]);
+            Console.WriteLine(arrayLinear[0]);
+            var keyPress = Console.ReadKey().KeyChar.ToString();
+
+        }
+    }
+}
