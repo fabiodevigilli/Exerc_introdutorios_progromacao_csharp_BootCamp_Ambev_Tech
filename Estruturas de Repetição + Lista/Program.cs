cd589758506dddfb43c6bb0d1f9b5580ddd57619
Leonardo Davi Tavares <55095964+LeoDavii@users.noreply.github.com>
2019-09-11 23:37:13 -0300

    Tentativa de Projeto

---
+++ Estruturas de Repetição + Lista/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estruturas_de_Repetição___Lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Var. para gravar a quantidade alunos a serem cadastrados e 
+              var. para concatenar com o nome do aluno */
+            
+            Console.WriteLine("Você deseja visualizar a tabuada de qual número? ");
+            var numTab = Convert.ToInt32(Console.ReadLine());
+             for (int i = 0; i <= numTab; i++)
+
+            {
+                var result = numTab * i;                
+                var texto1 = @"X{i} = {i}";
+                
+              var textoFinal = string.Format(Conso)
+
+            Console.ReadKey();
+            }
+        }
+    }
+}
