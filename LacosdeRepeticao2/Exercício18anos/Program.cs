a072f3c7ad4e345da4f95c117ef00a89f0d6dfb1
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 15:49:28 -0300

    BackUp

---
+++ LacosdeRepeticao2/Exercício18anos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercício18anos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            var listaNomes = "nome:Felipe,idade:27;nome:Giomar,idade:17;nome:Edson,idade:19;nome:Ericledson,idade:75;nome:Junior,idade:45";
+            var listaNomesSp = listaNomes.Split(';');
+           
+                 
+            foreach (var item in listaNomesSp)
+            {
+              
+                var infos = item.Split(',');
+            
+                var nome = infos[0].Split(':')[1];
+                var idade = int.Parse(infos[1].Split(':')[1]);
+                if (idade>=18)
+                 Console.WriteLine($"{nome},{idade}");
+                
+            }
+            Console.ReadKey();
+        }
+    }
+}
--- LacosdeRepeticao2/ExercícioNomeCarro/Program.cs
+++ LacosdeRepeticao2/ExercícioNomeCarro/Program.cs
@@ -16,27 +16,40 @@ namespace ExercícioNomeCarro
         }
         static void NomeCarro()
         {
+            // SALVA A LISTA DE INFORMAÇÕES
             var listaCarros = "carro:Gol,marca:volkswagen,ano:2000;carro:Jetta,marca:volkswagen,ano:2012;carro:Sportage,marca:Kia,ano:2011;carro:Hb20,marca:hyundai,ano:2015";
+            // SPLIT QUEBRA AS LINHAS DE ACORDO COM O CARACTER IDENTIFICADO ;.
+            // ESTÁ ASSINADO DENTRO DE UMA STRING QUE SERÁ UTILIZADA DE VETOR 
             var listaCarrosSpl = listaCarros.Split(';');
+            // FOREACH PERCORRE O VETOR COM A VARIÁVEL ITEM
             foreach (var item in listaCarrosSpl)
             {
+                //RETORNA A VARIÁVEL ITEM COM A PRIMEIRA INFORMAÇÃO ANTES DA PRIMEIRA VÍRGULA E IMPRIME NA TELA
                 Console.WriteLine(item.Split(',')[0]);
-                
+
             }
             Console.WriteLine("Informe o modelo do carro cujas informações devem ser exibidas:");
             var nomeBusca = Console.ReadLine();
 
+            //PERCORRE CADA ITEM DO VETOR
             foreach (var item in listaCarrosSpl)
             {
+                //QUEBRA A LINHA EM CADA VÍRGULA DO VETOR E GRAVA NA VARIÁVEL
                 var infos = item.Split(',');
+                //ASSINA A POSIÇÃO DE CADA VETOR, EM CADA VARIÁVEL, GRAVANDO APENAS O PRIMEIRO VALOR APÓS OS ":"
                 var nome = infos[0].Split(':')[1];
                 var marca = infos[1].Split(':')[1];
                 var ano = infos[2].Split(':')[1];
+                //COMPRARA SE A VARIÁVEL NOME ESTÁ DE ACORDO COM A INFORMAÇÃO BUSCADA
                 if (nomeBusca == nome)
                 {
+                    //IMPRIME AS DEMAIS INFORMAÇÕES DO CARRO BUSCADO
                     Console.WriteLine($"O {nome} é da {marca}, produzido em {ano}");
                 }
-           }
+            }
         }
+       
+            
+
     }
 }
