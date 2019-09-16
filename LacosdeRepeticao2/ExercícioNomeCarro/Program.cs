1d2fc1e91a42ba65574b70418c696cacee3ffaf2
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 12:05:10 -0300

    Backup 16.09

---
+++ LacosdeRepeticao2/ExercícioNomeCarro/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercícioNomeCarro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NomeCarro();
+            Console.ReadKey();
+
+        }
+        static void NomeCarro()
+        {
+            var listaCarros = "carro:Gol,marca:volkswagen,ano:2000;carro:Jetta,marca:volkswagen,ano:2012;carro:Sportage,marca:Kia,ano:2011;carro:Hb20,marca:hyundai,ano:2015";
+            var listaCarrosSpl = listaCarros.Split(';');
+            foreach (var item in listaCarrosSpl)
+            {
+                Console.WriteLine(item.Split(',')[0]);
+                
+            }
+            Console.WriteLine("Informe o modelo do carro cujas informações devem ser exibidas:");
+            var nomeBusca = Console.ReadLine();
+
+            foreach (var item in listaCarrosSpl)
+            {
+                var infos = item.Split(',');
+                var nome = infos[0].Split(':')[1];
+                var marca = infos[1].Split(':')[1];
+                var ano = infos[2].Split(':')[1];
+                if (nomeBusca == nome)
+                {
+                    Console.WriteLine($"O {nome} é da {marca}, produzido em {ano}");
+                }
+           }
+        }
+    }
+}
--- LacosdeRepeticao2/InicandoForEach/Program.cs
+++ LacosdeRepeticao2/InicandoForEach/Program.cs
@@ -10,8 +10,9 @@ namespace InicandoForEach
     {
         static void Main(string[] args)
         {
-            ForEachExercitar();
-           
+            ForEachComSplitList();
+            Console.ReadKey();
+
         }
         /// <summary>
         /// Reescrever o texto de forma vertical, navega sobre um array unidimensional de caracteres (string)
@@ -46,9 +47,14 @@ namespace InicandoForEach
             }
             Console.ReadKey();
     }
+
+        /// <summary>
+        /// Exercícios com split e informando o nome
+        /// </summary> 
         private static void ForEachExercitar()
         {
-            var conteudo = "EXERCICIO.DE.FIXAÇÃO.QUALQUER";
+            Console.WriteLine("Informe seu nome");
+            var conteudo = $"EXERCICIO.DE.FIXAÇÃO.QUALQUER.{Console.ReadLine()}";
             Console.WriteLine("Informe a palavra para realizar a busca: ");
             var palavra = Console.ReadLine();
             var conteudoSplit = conteudo.Split('.');
@@ -60,9 +66,38 @@ namespace InicandoForEach
                     Console.WriteLine("Palavra encontrada com sucesso!");
                                 
             }
-            Console.ReadKey();
+            
         }
+        /// <summary>
+        /// APRESENTA OS NOMES DOS USUÁRIOS CADASTRADOS
+        /// </summary>
+        private static void ForEachComSplitList()
+        {
+            var conteudo = "nome:Felipe,idade:27;nome:Giomar,idade:75;nome:Eusebio,idade:29";
+            var listaDeInformacoes = conteudo.Split(';');
+            Console.WriteLine("Usuários cadastrados: ");
+            foreach (var item in listaDeInformacoes)
+            {
+                Console.WriteLine(item.Split(',')[0]);
+                
+            }
+            Console.WriteLine("Informe um nome do sistema:");
+            var nomeBusca = Console.ReadLine();
 
+            foreach (var item in listaDeInformacoes)
 
+            {
+                var informacoes = item.Split(',');
+                var nome = informacoes[0].Split(':')[1];
+                var idade = informacoes[1].Split(':')[1];
+                if (nome == nomeBusca)
+                {
+                    Console.WriteLine($"O {nome} está com {idade} anos de idade.");
+                }
+
+            }
+
+        }
     }
+    
 }
