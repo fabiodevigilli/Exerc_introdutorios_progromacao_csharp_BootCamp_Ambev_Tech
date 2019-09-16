1f88ec743d6acaa409da7f2615518da4be9ea862
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 08:48:18 -0300

    backup

---
+++ LacosdeRepeticao2/InicandoForEach/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InicandoForEach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe o texto:");
+            var conteudoDoTexto = Console.ReadLine();
+
+            foreach (var item in conteudoDoTexto)
+
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+        }
+    }
+}
