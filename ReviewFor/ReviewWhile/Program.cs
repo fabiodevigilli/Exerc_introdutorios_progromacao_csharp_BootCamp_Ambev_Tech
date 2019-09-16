77b25db998c76a40e011e11307bfc0bf9584515d
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 08:41:16 -0300

    BACKUP

---
+++ ReviewFor/ReviewWhile/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReviewWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informar texto: ");
+            var conteudoDoTexto = Console.ReadLine();
+            var i = 0;
+        while (i < conteudoDoTexto.Length)
+            {
+                Console.WriteLine(conteudoDoTexto[i]);
+                i++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
