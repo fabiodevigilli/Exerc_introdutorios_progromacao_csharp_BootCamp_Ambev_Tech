58ff919d2a07e0c3a5dc9c74f2fb6b9e1ed45d74
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 09:00:32 -0300

    backup

--- LacosdeRepeticao2/InicandoForEach/Program.cs
+++ LacosdeRepeticao2/InicandoForEach/Program.cs
@@ -10,12 +10,21 @@ namespace InicandoForEach
     {
         static void Main(string[] args)
         {
+           
+        }
+        /// <summary>
+        /// Reescrever o texto de forma vertical, navega sobre um array unidimensional de caracteres (string)
+        /// </summary>
+        private static void IniciandoForEach01()
+        {
             Console.WriteLine("Informe o texto:");
             var conteudoDoTexto = Console.ReadLine();
 
             foreach (var item in conteudoDoTexto)
 
             {
+                if (item == 'e')
+                    continue;
                 Console.WriteLine(item);
             }
             Console.ReadKey();
