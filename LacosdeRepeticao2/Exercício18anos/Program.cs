8b147078b82e7fd72809649db44c47864ebea979
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-16 15:51:21 -0300

    backup 16/09

--- LacosdeRepeticao2/Exercício18anos/Program.cs
+++ LacosdeRepeticao2/Exercício18anos/Program.cs
@@ -9,23 +9,26 @@ namespace Exercício18anos
     class Program
     {
         static void Main(string[] args)
-        { 
-            var listaNomes = "nome:Felipe,idade:27;nome:Giomar,idade:17;nome:Edson,idade:19;nome:Ericledson,idade:75;nome:Junior,idade:45";
-            var listaNomesSp = listaNomes.Split(';');
-           
-                 
-            foreach (var item in listaNomesSp)
-            {
-              
-                var infos = item.Split(',');
-            
-                var nome = infos[0].Split(':')[1];
-                var idade = int.Parse(infos[1].Split(':')[1]);
-                if (idade>=18)
-                 Console.WriteLine($"{nome},{idade}");
-                
-            }
+        {
+            Listar18Anos();
             Console.ReadKey();
         }
-    }
+        private static void Listar18Anos()
+        { 
+        var listaNomes = "nome:Felipe,idade:27;nome:Giomar,idade:17;nome:Edson,idade:19;nome:Ericledson,idade:75;nome:Junior,idade:45";
+        var listaNomesSp = listaNomes.Split(';');
+
+
+            foreach (var item in listaNomesSp)
+            {
+
+                var infos = item.Split(',');
+
+                var nome = infos[0].Split(':')[1];
+                var idade = int.Parse(infos[1].Split(':')[1]);
+                if (idade >= 18)
+                    Console.WriteLine($"{nome},{idade}");
+            }      
+            }
+}
 }
