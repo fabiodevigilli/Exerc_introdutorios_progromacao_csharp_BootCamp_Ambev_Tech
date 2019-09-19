278fa5276e6dcb025cba299e645489d474b8c9a5
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-19 09:32:14 -0300

    backup 19.09

---
+++ Exercicio19.09/N2/Model/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N2.Model
+{
+    class Pessoa
+    {
+
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+        public char Genero { get; set; }
+        public double Altura { get; set; }
+    }
+}
---
+++ Exercicio19.09/N2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using N2.Model;
+
+
+namespace N2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public static void Formulario()
+        {
+            Console.WriteLine("Quantas pessoas você deseja cadastrar: ");
+            var qtdPessoas = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < qtdPessoas; i++)
+            {
+                Console.WriteLine("Qual o nome da pessoa?");
+                string nome = Console.ReadLine();
+                Console.WriteLine("Qual a idade da pessoa?");
+                var idade = int.Parse(Console.ReadLine());
+                Console.WriteLine(@"Qual o gênero desta pessoa? Digite:
+M - Masculino
+F - Feminino
+O - Outro");
+                var genero = Console.ReadKey().KeyChar;
+                Console.WriteLine("Qual a altura?");
+                var altura = double.Parse(Console.ReadLine());
+
+                var listaPessoas = new List<Pessoa>();
+                listaPessoas.Add(new Pessoa()
+                {
+                    Nome = nome,
+                    Idade = idade,
+                    Genero = genero,
+                    Altura = altura
+                });
+                                                                                 
+            }
+
+            
+
+
+        }
+    }
+}
+
+
+
+
+
+
