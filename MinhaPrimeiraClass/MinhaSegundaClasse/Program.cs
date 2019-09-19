ba919d55ae876412c10c69eb8f52cd6fb4aee3ea
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-19 08:16:31 -0300

    Backup 19.09

---
+++ MinhaPrimeiraClass/MinhaSegundaClasse/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MinhaSegundaClasse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var minhaCestaDeFrutas = new List<CestaDeFrutas>();
+
+            minhaCestaDeFrutas.Add(new CestaDeFrutas()
+            {
+                Nome = "Banana",
+                Quantidade = 5
+            });
+
+            minhaCestaDeFrutas.Add(new CestaDeFrutas()
+            {
+                Nome = "Tomate",
+                Quantidade = 3
+
+            });
+
+            minhaCestaDeFrutas.ForEach(i => Console.WriteLine($"Nome: {i.Nome} Quantidade: {i.Quantidade}"));
+            Console.ReadKey();
+
+        }
+        }
+    }
+
