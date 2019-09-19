5b2168977577c51b2c1ae03195864493eff8163c
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-19 08:43:42 -0300

    backup 19.09

---
+++ Exercicio19.09/N1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using N1.Model;
+
+namespace N1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var friendsList = new List<Amigos>();
+            {
+                friendsList.Add(new Amigos()
+                {
+                    Nome = "N1",
+                    TempoDeAmizade = 3
+                    
+
+                });
+                friendsList.Add(new Amigos()
+                {
+                    Nome = "N2",
+                    TempoDeAmizade = 5
+
+
+                });
+                friendsList.Add(new Amigos()
+                {
+                    Nome = "N3",
+                    TempoDeAmizade = 10
+
+
+                });
+                friendsList.ForEach(i => Console.WriteLine($"Nome: {i.Nome}, Tempo de amizade: {i.TempoDeAmizade}"));
+                Console.ReadKey();
+
+            }
+
+
+        }
+        
+  
+    }
+}
