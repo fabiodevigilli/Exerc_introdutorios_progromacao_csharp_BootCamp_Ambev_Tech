a3d68673eb39875fb20bae4a833bb674e2613317
Leonardo Davi Tavares <l.davi.t@outlook.com>
2019-09-20 11:29:59 -0300

    Backup 20.09

---
+++ SolucaoParaLocacaoDeLivros/InterfaceBiblioteca/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LocacaoBiblioteca.Controller;
+using LocacaoBiblioteca.Model;
+
+namespace InterfaceBiblioteca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("************************ SISTEMA DE LOCAÇÃO 1.0 *************************");
+            while (!RealizaLoginSistema())
+                Console.WriteLine("Login e senha inválidos");
+            MostraMenuSistema();
+            Console.ReadKey();
+           
+
+
+
+        }
+        /// <summary>
+        /// MOSTRA O MENU DO SISTEMA
+        /// </summary>
+        private static void MostraMenuSistema()
+        {
+            Console.Clear();
+            Console.WriteLine("************************ SISTEMA DE LOCAÇÃO 1.0 *************************");
+
+            Console.WriteLine("Menu Sistema");
+            Console.WriteLine("0 - Sair");
+            Console.WriteLine("1 - Listar Usuários");
+
+        }
+        /// <summary>
+         /// MÉTODO MOSTRA A TELA DE LOGIN E VALIDA AS INFORMAÇOES DE ACESSO
+         /// </summary>
+         /// <returns> VERDADEIRO PARA LOGIN E SENHA CORRETOS, FALSO PARA INCORRETOS.</returns>
+         /// 
+        private static bool RealizaLoginSistema()
+        {
+        Console.WriteLine("Informe seu login e senha para acessar o sistema:");
+
+            Console.WriteLine("Login:");
+            var loginDoUsuario = Console.ReadLine();
+
+        Console.WriteLine("Senha: ");
+            var senhaDoUsuario = Console.ReadLine();
+
+        UsuarioController usuarioController = new UsuarioController();
+            return usuarioController.LoginSistema(new Usuarios() { });
+           
+        }
+        
+    }
+}
---
+++ SolucaoParaLocacaoDeLivros/LocacaoBiblioteca/Controller/UsuarioController.cs
@@ -0,0 +1,30 @@
+﻿using LocacaoBiblioteca.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocacaoBiblioteca.Controller
+{
+    public class UsuarioController
+    {
+        /// <summary>
+        /// MÉTODO QUE REALIZA O LOGIN NO SISTEMA
+        /// LOGIN PADRÃO: Admin
+        /// SENHA PADRÃO: Admin
+        /// </summary>
+        /// <param name="login": LOGIN DO USUÁRIO></param>
+        /// <param name="senha": SENHA DO USUÁRIO</param>
+        /// <returns>RETORNA VERDADEIRO QUANDO EXISTIR O USUÁRIO COM ESTE LOGIN E SENHA</returns>
+        public bool LoginSistema(Usuarios usuarios)
+        {
+            if (usuarios.Login == "Admin" && usuarios.Senha == "Admin")
+                return true;
+
+            else
+                return false;
+        }
+
+    }
+}
